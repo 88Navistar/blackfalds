c389d173e5db141902e3470d0f4eed98499e16fb
Rob Wallator <63985483+88Navistar@users.noreply.github.com>
2025-02-11 17:10:34 -0700

    start design

--- nextjs-app/app/page.tsx
+++ nextjs-app/app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 import { Suspense } from "react";
 
@@ -6,10 +7,16 @@ import { AllPosts } from "@/components/Posts";
 export default async function Page() {
   return (
     <>
-      <div className="relative">
-        <p>Blackfalds</p>
-      </div>
       <div className="border-gray-10 border-t">
+        <div className="mx-auto px-4">
+          <Image
+            src="/images/street-scene-blackfalds.webp"
+            alt="Blackfalds"
+            className="mt-16 w-full rounded-sm shadow-lg"
+            width={1600}
+            height={1000}
+          />
+        </div>
         <div className="container">
           <aside className="py-12 sm:py-20">
             <Suspense fallback={null}>{await AllPosts()}</Suspense>
