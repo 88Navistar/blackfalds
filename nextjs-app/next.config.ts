6be984d77ae2eb686a1854a95263515819d822dd
Rob Wallator <63985483+88Navistar@users.noreply.github.com>
2025-02-21 15:15:14 -0700

    redirects

--- nextjs-app/next.config.ts
+++ nextjs-app/next.config.ts
@@ -18,6 +18,106 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: "/index.php",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/donation.php",
+        destination: "/about",
+        permanent: true,
+      },
+      {
+        source: "/trivia.php",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/location.php",
+        destination: "/contact",
+        permanent: true,
+      },
+      {
+        source: "/contact.php",
+        destination: "/contact",
+        permanent: true,
+      },
+      {
+        source: "/veterans.php",
+        destination: "/projects/blackfalds-veterans-memorial",
+        permanent: true,
+      },
+      {
+        source: "/faith.php",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/walkingtours.php",
+        destination:
+          "/projects/on-this-spot-blackfalds-history-at-your-fingertips",
+        permanent: true,
+      },
+      {
+        source: "/pdfs/Wigmore-Uniform-Project.pdf",
+        destination: "/projects/the-wigmore-uniform-project",
+        permanent: true,
+      },
+      {
+        source: "/mural.php",
+        destination: "/projects/iron-ridge-secondary-campus-mural",
+        permanent: true,
+      },
+      {
+        source: "/mural-gregson.php",
+        destination: "/projects/arthur-d-gregson-entomologist",
+        permanent: true,
+      },
+      {
+        source: "/mural-buffalotrail.php",
+        destination: "/projects/buffalo-lake-trail",
+        permanent: true,
+      },
+      {
+        source: "/mural-whitfordhouse.php",
+        destination: "/projects/whitford-stopping-house",
+        permanent: true,
+      },
+      {
+        source: "/mural-CEtrail.php",
+        destination: "/projects/c-and-e-trail",
+        permanent: true,
+      },
+      {
+        source: "/mural-early.php",
+        destination: "/projects/early-exploration-alberta",
+        permanent: true,
+      },
+      {
+        source: "/mural-Dam.php",
+        destination: "/projects/blindman-river-dam",
+        permanent: true,
+      },
+      {
+        source: "/mural-Name.php",
+        destination: "/projects/how-the-blindman-river-got-its-name",
+        permanent: true,
+      },
+      {
+        source: "/translations.php",
+        destination: "/resources/indigenous-translations",
+        permanent: true,
+      },
+      {
+        source: "/sources.php",
+        destination: "/resources/mural-sources",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
